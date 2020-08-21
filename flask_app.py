7c27f1e4a01100cc097bac306f023e10b92acde1
CCChenhao997 <ccchenhao997@gmail.com>
2020-08-22 00:43:28 +0800

    change app.py to flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,17 +1,12 @@
-import os
-import re
+# import os
+# import re
 import json
 from loguru import logger
 from predict import infer
 from config import Config
-from flask import Flask, render_template, request, jsonify, Response
+from flask import Flask, request, jsonify, Response
 
 app = Flask(__name__)
-
-# @app.route("/", methods=['GET'])
-# def index():
-#     return render_template('index.html')
-
 
 @app.route("/sa", methods=['POST'])
 def sa_predict():
@@ -21,13 +16,15 @@ def sa_predict():
             logger.info("接受到一个SA post请求, IP:{}.".format(request.remote_addr))
             if isinstance(input_data, bytes):
                 input_data = str(input_data, encoding='utf-8')
+            logger.info('input_data:{}'.format(input_data))
             input_json = json.loads(input_data)
             # QAanswer, graph_info = engine.answer(input_json['text'])
             predict_tags, _ = infer.evaluate(input_json['text'])
-            predict_tags = predict_tags[0]
+            # predict_tags = predict_tags[0]
 
             base = 0
             aspect_layer1_dict = {}
+            score_list = []
             for aspect in Config.aspect_names:
                 tags = []
                 score = 60
@@ -48,11 +45,13 @@ def sa_predict():
                     # tags.append(predict_tags[i])
                 aspect_layer1_dict.update({aspect: score})
                 base += len(Config.aspect_names[aspect])
+                score_list.append(score)
 
             predict_tags_trans = [Config.label_trans(i) for i in predict_tags]
             aspect_layer2_dict = dict(zip(Config.label_names, predict_tags_trans))
             
-            data = {'Aspect_first_layer': aspect_layer1_dict, 'Aspect_second_layer': aspect_layer2_dict}
+            data = {'Aspect_first_layer': aspect_layer1_dict, 'Aspect_second_layer': aspect_layer2_dict, 'score_list': score_list}
+            print(data)
             # return jsonify({'predict_tags': aspect_label})
             return Response(json.dumps(data, sort_keys=False, indent=4, ensure_ascii=False), mimetype='application/json')
         else:
